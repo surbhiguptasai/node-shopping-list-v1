262a63c5d0790fc5ba1a186df1df69e51f700765
Surbhi83 <surabhi.gupta.sai@gmail.com>
2017-04-28 22:22:11 -0400

    recipes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
-
+const {Recipes} = require('./models');
 const jsonParser = bodyParser.json();
 const app = express();
 
@@ -17,11 +17,15 @@ app.use(morgan('common'));
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
